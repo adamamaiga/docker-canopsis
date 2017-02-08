229543b8485d9d86593070227785d609e7e1f60e
Adama Maïga <adamamaiga@users.noreply.github.com>
2017-02-08 13:25:34 +0100

    [FIX] CollectD unable to connect amqp plugin
    FIX - [CollectD] Error: unable to connect amqp plugin: amqp_login (vhost = canopsis, user = cpsrabbit) failed

--- start.sh
+++ start.sh
@@ -10,4 +10,13 @@ do
     RESULT=`/opt/canopsis/etc/init.d/canopsis status`
     STATUS=`echo ${RESULT} | egrep "STOPPED|STARTING"`
 done
+
+#~ FIX - [CollectD] Error: unable to connect amqp plugin: amqp_login (vhost = canopsis, user = cpsrabbit) failed
+# Declared VHost
+/opt/canopsis/bin/rabbitmqadmin declare vhost name=canopsis
+# Declared User
+/opt/canopsis/bin/rabbitmqadmin declare user name=cpsrabbit password=canopsis tags=administrator
+# Set user permission
+/opt/canopsis/bin/rabbitmqadmin declare permission vhost=canopsis user=cpsrabbit configure=.* write=.* read=.*
+
 tail -f /opt/canopsis/var/log/*
